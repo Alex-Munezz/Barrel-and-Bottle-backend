fba6b38ed00a83a89302a7eebdd434066b18c779
veronicahmbuvi <mbuviveronicah3@gmail.com>
2023-07-05 22:29:38 +0300

    working on crud operations

--- app/app.py
+++ app/app.py
@@ -1,15 +1,63 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS
+from flask import Flask, request, make_response, jsonify
 
+from flask_migrate import Migrate
+from models import db, Review,Drink
 app = Flask(__name__)
-CORS(app)
+# CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+app.json.compact = False
+# db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
+db.init_app(app)
+
+@app.route('/drinks', methods=['GET'])
+def get_drinks():
+    drinks = Drink.query.all()
+    drinks_list = []
+    for drink in drinks:
+        drinks_data = {
+            'id': drink.id,
+            'cover':drink.cover,
+            'name': drink.name,
+            'percentage': drink.percentage,
+            'breweries': drink.breweries,
+            'price':drink.price
+            
+        }
+        drinks_list.append(drinks_data)
+    return jsonify(drinks_list)
+
+@app.route('/reviews', methods=['GET'])
+def get_reviews():
+    reviews = Review.query.all()
+    reviews_list = []
+    for review in reviews:
+        reviews_data = {
+            'id': review.id,
+            'drink_id': review.drink_id,
+            'customer_id': review.customer_id,
+            'review': review.review
+            
+        }
+        reviews_list.append(reviews_data)
+    return jsonify(reviews_list)
+
+@app.route('/drinks/<int:drink_id>', methods=['DELETE'])
+def delete_drink(drink_id):
+    drink = Drink.query.get(drink_id)
+    if drink:
+        db.session.delete(drink)
+        db.session.commit()
+        return '', 204
+    else:
+        return jsonify({'error': 'Drink not found'}), 404
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=5555)
 
-from routes import reviews_blueprint
-app.register_blueprint(reviews_blueprint)
+
+
+# from routes import reviews_blueprint
+# app.register_blueprint(reviews_blueprint)
--- app/models.py
+++ app/models.py
@@ -1,126 +1,34 @@
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import ForeignKey
-from sqlalchemy.orm import relationship
-
-
-db = SQLAlchemy()
-
-def create_all_tables():
-    from app import app
-    db.init_app(app)
-    with app.app_context():
-        db.create_all()
-
-from app import db
-
-class Drink(db.Model):
-    __tablename__ = 'drinks'
-    id = db.Column(db.Integer, primary_key=True)
-    cover = db.Column(db.String(100000), unique=True)
-    name = db.Column(db.String, unique=True)
-    percentage = db.Column(db.Integer)
-    breweries = db.Column(db.String)
-    price = db.Column(db.Integer)
-
-    reviews = db.relationship('Review', backref='drink', lazy='dynamic')
-    sales = db.relationship('Sale', backref='drink', cascade='all, delete-orphan')
-
-    def __init__(self, id, cover, name, percentage, breweries, price):
-        self.id = id
-        self.cover = cover
-        self.name = name
-        self.percentage = percentage
-        self.breweries = breweries
-        self.price = price
-
-
-class Review(db.Model):
-    __tablename__ = 'reviews'
-
-    id = db.Column(db.Integer, primary_key=True)
-    drink_id = db.Column(db.Integer, db.ForeignKey('drinks.id'), nullable=False)
-    # drink_id = db.Column(db.Integer, nullable=False)
-    customer_id = db.Column(db.Integer, ForeignKey('customers.id'))
-    review = db.Column(db.String(255), nullable=False)
-
-    # drink_id = db.Column(db.Integer, ForeignKey('drinks.id'), nullable=False)
-    def __init__(self, drink_id, customer_id, review):
-        self.drink_id = drink_id
-        self.customer_id = customer_id
-        self.review = review
-    
-
-
-class Customer(db.Model):
-    __tablename__ = 'customers'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String)
-    email_address = db.Column(db.String)
-    password = db.Column(db.String)
-
-    reviews = relationship('Review', backref='customer')
-    sales = db.relationship('Sale', backref='customer', cascade='all, delete-orphan')
-
-    def __init__(self, username, email_address, password):
-        self.username = username
-        self.email_address = email_address
-        self.password = password
-
-
-class Admin(db.Model):
-    __tablename__ = 'admins'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String)
-    password = db.Column(db.String)
-
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
-
-
-class Sale(db.Model):
-    __tablename__ = 'sales'
-    id = db.Column(db.Integer, primary_key=True)
-    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'))
-    drink_id = db.Column(db.Integer, db.ForeignKey('drinks.id'))
-
-    def __init__(self, customer_id, drink_id):
-        self.customer_id = customer_id
-        self.drink_id = drink_id
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 # from flask_sqlalchemy import SQLAlchemy
+# from sqlalchemy import ForeignKey
+# from sqlalchemy.orm import relationship
+
 
 # db = SQLAlchemy()
+
+# def create_all_tables():
+#     from app import app
+#     db.init_app(app)
+#     with app.app_context():
+#         db.create_all()
+
+# from app import db
 
 # class Drink(db.Model):
 #     __tablename__ = 'drinks'
 #     id = db.Column(db.Integer, primary_key=True)
+#     
 #     name = db.Column(db.String, unique=True)
 #     percentage = db.Column(db.Integer)
 #     breweries = db.Column(db.String)
 #     price = db.Column(db.Integer)
 
-#     reviews = db.relationship('Review', backref='drink')
-#     sales = db.relationship('Sale', backref='drink')
+#     reviews = db.relationship('Review', backref='drink', lazy='dynamic')
+#     sales = db.relationship('Sale', backref='drink', cascade='all, delete-orphan')
 
-#     def __init__(self, name, type, percentage, breweries, price):
+#     def __init__(self, id, cover, name, percentage, breweries, price):
+#         self.id = id
+#         self.cover = cover
 #         self.name = name
-#         self.type = type
 #         self.percentage = percentage
 #         self.breweries = breweries
 #         self.price = price
@@ -128,13 +36,19 @@ class Sale(db.Model):
 
 # class Review(db.Model):
 #     __tablename__ = 'reviews'
-#     id = db.Column(db.Integer, primary_key=True)
-#     drink_id = db.Column(db.Integer, db.ForeignKey("drinks.id"))
-#     customer_id = db.Column(db.Integer, db.ForeignKey("customers.id"))
-#     review = db.Column(db.String)
 
-#     def __init__(self, review):
+#     id = db.Column(db.Integer, primary_key=True)
+#     drink_id = db.Column(db.Integer, db.ForeignKey('drinks.id'), nullable=False)
+#     # drink_id = db.Column(db.Integer, nullable=False)
+#     customer_id = db.Column(db.Integer, ForeignKey('customers.id'))
+#     review = db.Column(db.String(255), nullable=False)
+
+#     # drink_id = db.Column(db.Integer, ForeignKey('drinks.id'), nullable=False)
+#     def __init__(self, drink_id, customer_id, review):
+#         self.drink_id = drink_id
+#         self.customer_id = customer_id
 #         self.review = review
+    
 
 
 # class Customer(db.Model):
@@ -144,8 +58,8 @@ class Sale(db.Model):
 #     email_address = db.Column(db.String)
 #     password = db.Column(db.String)
 
-#     reviews = db.relationship('Review', backref='customer')
-#     sales = db.relationship('Sale', backref='customer')
+#     reviews = relationship('Review', backref='customer')
+#     sales = db.relationship('Sale', backref='customer', cascade='all, delete-orphan')
 
 #     def __init__(self, username, email_address, password):
 #         self.username = username
@@ -167,10 +81,97 @@ class Sale(db.Model):
 # class Sale(db.Model):
 #     __tablename__ = 'sales'
 #     id = db.Column(db.Integer, primary_key=True)
-#     customer_id = db.Column(db.Integer, db.ForeignKey("customers.id"))
-#     drink_id = db.Column(db.Integer, db.ForeignKey("drinks.id"))
+#     customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'))
+#     drink_id = db.Column(db.Integer, db.ForeignKey('drinks.id'))
 
 #     def __init__(self, customer_id, drink_id):
 #         self.customer_id = customer_id
 #         self.drink_id = drink_id
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Drink(db.Model):
+    __tablename__ = 'drinks'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+    cover = db.Column(db.String(100000), unique=True)
+    percentage = db.Column(db.Integer)
+    breweries = db.Column(db.String)
+    price = db.Column(db.Integer)
+
+    reviews = db.relationship('Review', backref='drink')
+    sales = db.relationship('Sale', backref='drink')
+
+    def __init__(self, name, type, percentage, breweries, price):
+        self.name = name
+        self.type = type
+        self.percentage = percentage
+        self.breweries = breweries
+        self.price = price
+
+
+class Review(db.Model):
+    __tablename__ = 'reviews'
+    id = db.Column(db.Integer, primary_key=True)
+    drink_id = db.Column(db.Integer, db.ForeignKey("drinks.id"))
+    customer_id = db.Column(db.Integer, db.ForeignKey("customers.id"))
+    review = db.Column(db.String)
+
+    def __init__(self, review):
+        self.review = review
+
+
+class Customer(db.Model):
+    __tablename__ = 'customers'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String)
+    email_address = db.Column(db.String)
+    password = db.Column(db.String)
+
+    reviews = db.relationship('Review', backref='customer')
+    sales = db.relationship('Sale', backref='customer')
+
+    def __init__(self, username, email_address, password):
+        self.username = username
+        self.email_address = email_address
+        self.password = password
+
+
+class Admin(db.Model):
+    __tablename__ = 'admins'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String)
+    password = db.Column(db.String)
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+
+class Sale(db.Model):
+    __tablename__ = 'sales'
+    id = db.Column(db.Integer, primary_key=True)
+    customer_id = db.Column(db.Integer, db.ForeignKey("customers.id"))
+    drink_id = db.Column(db.Integer, db.ForeignKey("drinks.id"))
+
+    def __init__(self, customer_id, drink_id):
+        self.customer_id = customer_id
+        self.drink_id = drink_id
 
